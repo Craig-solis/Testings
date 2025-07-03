1b858813b6af0765c958dc50f7d47dd81bcab33b
Craig-solis <csolis5454@gmail.com>
2025-07-03 11:49:05 -0500

    Update

--- Pages/firebase-config.js
+++ Pages/firebase-config.js
@@ -1,6 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  // ... other config
+  apiKey: "AIzaSyCmWe5bJF9m6gRE4sg5WaiCs6wPrE21Qmg",
+  authDomain: "frostbyte-test-6f9ae.firebaseapp.com",
+  projectId: "frostbyte-test-6f9ae",
+  storageBucket: "frostbyte-test-6f9ae.firebasestorage.app",
+  messagingSenderId: "958508912848",
+  appId: "1:958508912848:web:301b2cfa7d01d3185a8ed5",
+  measurementId: "G-QMHETXHVJY"
 };
+
 firebase.initializeApp(firebaseConfig);
